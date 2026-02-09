046528398a6a676d68a90431501a4ae93cbc7955
nikolaykaz70 <Nikolaykaz.70@mail.ru>
2026-02-09 09:09:02 +0200

    Create twrp_WP27.mk

---
+++ twrp_WP27.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_64bit.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := WP27
+PRODUCT_NAME := twrp_WP27
+PRODUCT_BRAND := Oukitel
+PRODUCT_MODEL := WP27
+PRODUCT_MANUFACTURER := Oukitel
