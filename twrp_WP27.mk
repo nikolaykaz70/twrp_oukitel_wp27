30ee04793b75fba99c0e636b134dcee7e84a5e83
nikolaykaz70 <Nikolaykaz.70@mail.ru>
2026-02-09 09:17:10 +0200

    Update twrp_WP27.mk

--- twrp_WP27.mk
+++ twrp_WP27.mk
@@ -1,8 +1,15 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_64bit.mk)
+# Наследуем базовые настройки (исправленный путь)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Наследуем конфигурацию TWRP
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Информация об устройстве
 PRODUCT_DEVICE := WP27
 PRODUCT_NAME := twrp_WP27
 PRODUCT_BRAND := Oukitel
 PRODUCT_MODEL := WP27
 PRODUCT_MANUFACTURER := Oukitel
+
+PRODUCT_GMS_CLIENTID_BASE := android-oukitel
